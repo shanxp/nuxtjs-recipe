c7ea7f142a26622b654001b8eb2b27004caad256
Shan Shanmugaesan <shan.shanmugaesan@ontrackms.com>
2025-10-16 20:59:59 +0100

    Prisma ORM ES6 module error fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  build: {
+    transpile: ['.prisma/client'],
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/google-fonts',
